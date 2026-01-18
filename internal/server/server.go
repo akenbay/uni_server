b72446a519ebb933623d5e369ee424c8f7154e00
Aibar <aibarkenbay@gmail.com>
2026-01-19 00:09:32 +0500

    Feat: add logger and recover to the server

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 	"university/internal/handler"
 )
@@ -17,6 +18,11 @@ func NewServer(handler *handler.Handler) *Server {
 func (s *Server) Start(addr string) error {
 	e := echo.New()
 
+	// Add global middleware
+	e.Use(middleware.Recover())
+	e.Use(middleware.RequestLogger())
+
+	// Register all routes (public and protected)
 	s.handler.Register(e)
 
 	return e.Start(addr)
